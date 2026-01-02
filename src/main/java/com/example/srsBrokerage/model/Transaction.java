6882abd15d4f838c1b0dde8a255a013607ed71bb
SeresZoltan <seres.zoltan.pers@gmail.com>
2026-01-02 23:17:49 +0200

    Add new entity TransactionEntry for starting double-entry ledger

--- src/main/java/com/example/srsBrokerage/model/Transaction.java
+++ src/main/java/com/example/srsBrokerage/model/Transaction.java
@@ -2,12 +2,11 @@ package com.example.srsBrokerage.model;
 
 import com.example.srsBrokerage.enums.TransactionType;
 import jakarta.persistence.*;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "transaction", schema = "accounts")
@@ -17,20 +16,9 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "account_id", nullable = false)
-    private Account account;
-
     @Column(name = "type", nullable = false, length = 50)
     @Enumerated(EnumType.STRING)
     private TransactionType transactionType;
-
-    @Column(name = "amount", nullable = false, precision = 19, scale = 4)
-    @ColumnDefault("0.0000")
-    private BigDecimal transactionAmount;
-
-    @Column(name = "currency", nullable = false, length = 3)
-    private String transactionCurrency;
 
     @Column(name = "description")
     private String transactionDescription;
@@ -43,21 +31,25 @@ public class Transaction {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
+    @OneToMany(mappedBy = "transaction", cascade = CascadeType.ALL)
+    private List<TransactionEntry> transactionEntries;
+
+    public void addTransactionEntry(TransactionEntry entry) {
+        transactionEntries.add(entry);
+        entry.setTransaction(this);
+    }
+
 
     public Transaction() {}
     public Transaction(
             Long id,
             TransactionType transactionType,
-            BigDecimal transactionAmount,
-            String transactionCurrency,
             String transactionDescription,
             LocalDateTime createdAt,
             LocalDateTime updatedAt
     ) {
         this.id = id;
         this.transactionType = transactionType;
-        this.transactionAmount = transactionAmount;
-        this.transactionCurrency = transactionCurrency;
         this.transactionDescription = transactionDescription;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
@@ -67,17 +59,8 @@ public class Transaction {
     public Long getId() {
         return id;
     }
-    public Account getAccount() {
-        return account;
-    }
     public TransactionType getTransactionType() {
         return transactionType;
-    }
-    public BigDecimal getTransactionAmount() {
-        return transactionAmount;
-    }
-    public String getTransactionCurrency() {
-        return transactionCurrency;
     }
     public String getTransactionDescription() {
         return transactionDescription;
@@ -93,17 +76,8 @@ public class Transaction {
     public void setId(Long id) {
         this.id = id;
     }
-    public void setAccount(Account account) {
-        this.account = account;
-    }
     public void setTransactionType(TransactionType transactionType) {
         this.transactionType = transactionType;
-    }
-    public void setTransactionAmount(BigDecimal transactionAmount) {
-        this.transactionAmount = transactionAmount;
-    }
-    public void setTransactionCurrency(String transactionCurrency) {
-        this.transactionCurrency = transactionCurrency;
     }
     public void setTransactionDescription(String transactionDescription) {
         this.transactionDescription = transactionDescription;
@@ -119,14 +93,11 @@ public class Transaction {
     @Override
     public String toString() {
         return "Transaction{" +
-                "Transaction ID =" + id +
-                ", User Account =" + account +
-                ", Transaction Type ='" + transactionType + '\'' +
-                ", Transaction Amount =" + transactionAmount +
-                ", Transaction Currency ='" + transactionCurrency + '\'' +
-                ", Transaction Description ='" + transactionDescription + '\'' +
-                ", Created At =" + createdAt +
-                ", Updated At =" + updatedAt +
+                "id=" + id +
+                ", transactionType=" + transactionType +
+                ", transactionDescription='" + transactionDescription + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
                 '}';
     }
 }
---
+++ src/main/java/com/example/srsBrokerage/model/TransactionEntry.java
@@ -0,0 +1,118 @@
+package com.example.srsBrokerage.model;
+
+import com.example.srsBrokerage.enums.Currency;
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transaction_entry", schema = "accounts")
+public class TransactionEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "transaction_id")
+    private Transaction transaction;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @Column(name = "amount", nullable = false, precision = 19, scale = 4)
+    @ColumnDefault("0.0000")
+    private BigDecimal transactionAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "currency", nullable = false, length = 3)
+    private Currency transactionCurrency;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+    public TransactionEntry() {}
+    public TransactionEntry(
+            Long id,
+            Transaction transaction,
+            Account account,
+            BigDecimal transactionAmount,
+            Currency transactionCurrency,
+            LocalDateTime createdAt
+    ) {
+        this.id = id;
+        this.transaction = transaction;
+        this.account = account;
+        this.transactionAmount = transactionAmount;
+        this.transactionCurrency = transactionCurrency;
+        this.createdAt = createdAt;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public  BigDecimal getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public Currency getTransactionCurrency() {
+        return transactionCurrency;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public void setTransactionAmount(BigDecimal transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public void setTransactionCurrency(Currency transactionCurrency) {
+        this.transactionCurrency = transactionCurrency;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+    @Override
+    public String toString() {
+        return "TransactionEntry{" +
+                "id=" + id +
+                ", transaction=" + transaction +
+                ", account=" + account +
+                ", transactionAmount=" + transactionAmount +
+                ", transactionCurrency=" + transactionCurrency +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
