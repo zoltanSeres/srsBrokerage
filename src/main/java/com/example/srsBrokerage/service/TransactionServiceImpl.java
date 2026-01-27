f5be8d05c65903daa6f4ff76128ee13990eb3624
SeresZoltan <seres.zoltan.pers@gmail.com>
2026-01-27 10:12:28 +0200

    enforced role-based rules in account deposit/withrawal/transfer

--- src/main/java/com/example/srsBrokerage/service/TransactionServiceImpl.java
+++ src/main/java/com/example/srsBrokerage/service/TransactionServiceImpl.java
@@ -228,7 +228,7 @@ public class TransactionServiceImpl implements TransactionService{
                 .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
 
         if (!loggedUserId.equals(accountId) && !isAdmin) {
-            throw new AccessForbiddenException("Can't perform this action.");
+            throw new AccessForbiddenException("Can't perform this action.");`11
         }
 
         List<Transaction> transactions =
